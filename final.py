caf09eaff8a9e2c82efd3322bf5d2aac2f3318a9
Aarish Iyer <aarish407@gmail.com>
2020-06-07 17:25:35 -0400

    Update final.py

--- final.py
+++ final.py
@@ -11,25 +11,21 @@ from GenerateEmbeddings import generate_embeddings_narrow, generate_embeddings_w
 input_dir= sys.argv[1]
 output_dir= sys.argv[2]
 
-
 # Read the two datasets
 input_path_narrow= input_dir + '/dataset-narrow'
-dataset_narrow= load_dataset(input_path_narrow)
-
 input_path_wide= input_dir + '/dataset-wide'
-dataset_wide= load_dataset(input_path_wide)
 
 # Create two subfolders within output dir, dataset-narrow and dataset-wide
 output_path_narrow= output_dir + '/dataset-narrow'
 output_path_wide= output_dir + '/dataset-wide'
 
 os.mkdir(output_path_narrow)
-os.mkdir(output_path_wide)
+# os.mkdir(output_path_wide)
 
+dataset_narrow= load_dataset(input_path_narrow)
+generate_embeddings_narrow(dataset_narrow[10:15], output_path_narrow)
+del dataset_narrow
 
-# print(dataset_narrow[1060:1070][1])
-# print(dataset_wide[1060:1070][1])
-# Generate Embeddings and predict
-generate_embeddings_narrow(dataset_narrow, output_path_narrow)
-generate_embeddings_wide(dataset_wide, output_path_wide)
-
+# dataset_wide= load_dataset(input_path_wide)
+# generate_embeddings_wide(dataset_wide[:2], output_path_wide)
+# del dataset_wide
